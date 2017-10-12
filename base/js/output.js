d69f1e93be3938a200303241f741c476b753dcf3
Anya2ka <annabassova152@gmail.com>
2017-10-12 13:15:22 +0300

    Added js outputs
    This closes #24.
    Work with basic output method in js:
     -alert,confirm, prompt;
     -document, console.

---
+++ base/js/output.js
@@ -0,0 +1,10 @@
+//Object Methods
+var age = prompt('Please, write your age', '');
+alert('you are ' + age);
+var result = confirm("Are you sure want to exit?");
+
+//JavaScript Display Possibilities
+console.log(age);
+function myFunction () {
+	document.write('<img src="https://3.bp.blogspot.com/-x3YRnY7M744/UXxmpUVlKqI/AAAAAAAAOAc/7livVYVgLOc/s1600/smajlik_1.png"/>');
+}
